b776ef14eaee3ba5439d1d79582b70421cd852a0
hitazuki <z522394547@gmali.com>
2019-01-05 01:01:01 +0800

    add main,print,alop interper

--- Mini_Java/src/ErrorDetection/RuntimeInterpreter.java
+++ Mini_Java/src/ErrorDetection/RuntimeInterpreter.java
@@ -18,14 +18,91 @@ public class RuntimeInterpreter {
             return 0;
         }
         else if(node.getClass()==A_MainClass.class){
+            Absyn stmt = ((A_MainClass)node).stmt;
+            int res = interpret(stmt);
+            return res;
+        }
+        else if(node.getClass()==A_ClassDec.class){
+
+        }
+        else if(node.getClass()==A_VarDec.class){
+
+        }
+        else if(node.getClass()==A_MethodDec.class){
+
+        }
+        else if(node.getClass()==A_Block.class){
+
+        }
+        else if(node.getClass()==A_If.class){
+
+        }
+        else if(node.getClass()==A_While.class){
+
+        }
+        else if(node.getClass()==A_Print.class){
+            int res = interpret(((A_Print)node).exp);
+            System.out.println(res);
             return 0;
         }
+        else if(node.getClass()==A_Assign.class){
 
-        //...
-        //...
-        //else if(){}
-        //...
+        }
+        else if(node.getClass()==A_AssignArray.class){
 
+        }
+        else if(node.getClass()==A_OpExp.class){
+            A_Oper oper = ((A_OpExp)node).oper;
+            Absyn left = ((A_OpExp)node).left;
+            Absyn right = ((A_OpExp)node).right;
+            int res_l = interpret(left);
+            int res_r = interpret(right);
+            if(oper == A_Oper.and){
+                return res_l & res_r;
+            }
+            else if(oper == A_Oper.minus){
+                return res_l - res_r;
+            }
+            else if(oper == A_Oper.plus){
+                return res_l + res_r;
+            }
+            else if(oper == A_Oper.times){
+                return res_l * res_r;
+            }
+            else if(oper == A_Oper.lt){
+                return res_l + res_r;
+            }
+        }
+        else if(node.getClass()==A_ArrayIndex.class){
+
+        }
+        else if(node.getClass()==A_ArrayLen.class){
+
+        }
+        else if(node.getClass()==A_CallExp.class){
+
+        }
+        else if(node.getClass()==A_IntExp.class){
+
+        }
+        else if(node.getClass()==A_BoolExp.class){
+
+        }
+        else if(node.getClass()==A_IdExp.class){
+
+        }
+        else if(node.getClass()==A_This.class){
+
+        }
+        else if(node.getClass()==A_NewArray.class){
+
+        }
+        else if(node.getClass()==A_NewObj.class){
+
+        }
+        else if(node.getClass()==A_NotExp.class){
+
+        }
         else{
             return 0;
         }
